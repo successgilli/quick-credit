b0cfe5b4ddfa6681d3d3ae9081a55e4d56e97df2
successgilli <42284402+successgilli@users.noreply.github.com>
2019-05-05 16:29:46 +0100

    Revert "[finishes #165746525] completed admin verify route and made routs fol…"

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { auth, user } from './user';
-
-const router = express.Router();
-
-router.use('/auth', auth);
-router.use('/users', user);
-
-export default router;
