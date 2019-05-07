108315af59095cdd102f263dc97ce8a286e0521d
successgilli <successgilli@gmail.com>
2019-05-07 10:33:33 +0100

    [finishes #165747457] completed Get repayment history route data structure

--- server/routes/loan.js
+++ server/routes/loan.js
@@ -4,11 +4,12 @@ import Loan from '../controllers/loanController';
 
 const loan = express.Router();
 const { loanApplyValidator, laonStatusValidator, loanRepaymentValidator } = DataCreationValidator;
-const { apply, getLoan, changeStatus, postRepayment } = Loan;
+const { apply, getLoan, changeStatus, postRepayment, getRepayHistory } = Loan;
 
 loan.post('/', loanApplyValidator, apply);
 loan.get('/:loanId([0-9]+)', getLoan);
 loan.patch('/:loanId([0-9]+)', laonStatusValidator, changeStatus);
-loan.post('/:loanId([0-9]+)/repayment', loanRepaymentValidator, postRepayment)
+loan.post('/:loanId([0-9]+)/repayment', loanRepaymentValidator, postRepayment);
+loan.get('/:loanId([0-9]+)/repayments', getRepayHistory);
 
 export default loan;
