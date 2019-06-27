c9edcc19d84eb74e18c70d6dc0fcb402b26dc4ae
successgilli <successgilli@gmail.com>
2019-06-27 18:31:19 +0100

    fetch user pictures(feature):user images
    - ensure user gets pictures when window loads
    
    [finishes #166954044]

--- UI/js/userProfile.js
+++ UI/js/userProfile.js
@@ -6,23 +6,35 @@ const updatePix = document.getElementById('updatePix');
 const aside = document.getElementById('aside');
 const userMainContainer = document.getElementById('userMainContainer');
 const profilePicture = document.getElementById('profilePicture');
+const loader = document.getElementById('loader');
 
 updatePix.addEventListener('change', () => {
+  const userImage = document.getElementById('userImage');
   const url = 'https://quickcreditgilli.herokuapp.com/api/v1/users/uploads';
   console.log('entered');
   const formData = new FormData();
   formData.append('image', updatePix.files[0]);
   console.log(formData);
+  loader.style.opacity = '1';
   const request = new Request(url, {
     method: 'PATCH',
     headers: new Headers({ Authorization: localStorage.getItem('auth') }),
     body: formData,
   });
-  fetch(request).then(res => res.json())
-    .then((obj) => {
-      console.log(obj);
-      profilePicture.setAttribute('src', obj.data.passporturl)
-    });
+  fetch(request).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    throw Error(res.statusText);
+  }).then((obj) => {
+    console.log(obj);
+    profilePicture.setAttribute('src', obj.data.passporturl);
+    userImage.setAttribute('src', obj.data.passporturl);
+    loader.style.opacity = '0';
+  }).catch((er) => {
+    console.log(er);
+    loader.style.opacity = '0';
+  });
 });
 // add event to navbar on large screen
 navWide.addEventListener('click', () => {
@@ -61,4 +73,44 @@ navUser.addEventListener('click', () => {
       userAsideBackground.style.display = 'none';
     }, 500);
   }
+});
+// add event listener to load user page
+window.addEventListener('load', () => {
+  const userName = document.getElementById('userName');
+  const userEmail = document.getElementById('userEmail');
+  const userAccount = document.getElementById('userAccount');
+  const userAddress = document.getElementById('userAddress');
+  const userImage = document.getElementById('userImage');
+  const url = 'https://quickcreditgilli.herokuapp.com/api/v1/users';
+  const request = new Request(url, {
+    method: 'GET',
+    headers: new Headers({
+      'Content-type': 'application/json',
+      Authorization: localStorage.getItem('auth'),
+      Accept: 'application/json,text/plain,*/*',
+    }),
+  });
+  loader.style.opacity = '1';
+  fetch(request).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    throw Error(res.statusText);
+  }).then((obj) => {
+    loader.style.opacity = '0';
+    if (obj.data.passporturl === null) {
+      profilePicture.setAttribute('src', './img/profile-placeholder.gif');
+      userImage.setAttribute('src', './img/profile-placeholder.gif');
+    } else {
+      profilePicture.setAttribute('src', obj.data.passporturl);
+      userImage.setAttribute('src', obj.data.passporturl);
+    }
+    userAddress.textContent = obj.data.address;
+    userName.textContent = `${obj.data.firstname} ${obj.data.lastname}`;
+    userEmail.textContent = obj.data.email;
+    userAccount.textContent = obj.data.accountnumber;
+  }).catch((err) => {
+    loader.style.opacity = '1';
+    console.log(err);
+  });
 });
