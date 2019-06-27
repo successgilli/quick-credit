8b2545e7eafc9bf0bd2c9a1639f4f4f335de052a
successgilli <successgilli@gmail.com>
2019-06-27 18:31:19 +0100

    fetch user pictures(feature):user images
    - ensure user gets pictures when window loads
    
    [finishes #166954044]

--- js/userDashboard.js
+++ js/userDashboard.js
@@ -284,5 +284,31 @@ const windowLoad = () => {
     }).catch(err => console.log(err));
   });
 };
-
+// add event to load user pix
+window.addEventListener('load', () => {
+  const userImage = document.getElementById('userImage');
+  const url = 'https://quickcreditgilli.herokuapp.com/api/v1/users';
+  const request = new Request(url, {
+    method: 'GET',
+    headers: new Headers({
+      'Content-type': 'application/json',
+      Authorization: localStorage.getItem('auth'),
+      Accept: 'application/json,text/plain,*/*',
+    }),
+  });
+  fetch(request).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    throw Error(res.statusText);
+  }).then((obj) => {
+    if (obj.data.passporturl === null) {
+      userImage.setAttribute('src', './img/profile-placeholder.gif');
+    } else {
+      userImage.setAttribute('src', obj.data.passporturl);
+    }
+  }).catch((err) => {
+    console.log(err);
+  });
+});
 windowLoad();
